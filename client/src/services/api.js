523d522e449df3f92dd55b5ea7815c47e351ba18
Abijith C.G <148551049+abimad123@users.noreply.github.com>
2025-12-14 20:12:22 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-
-const API_URL = 'http://localhost:5000/api';
+// Vite requires variables to start with "import.meta.env"
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 const getAuthHeaders = () => {
   const token = localStorage.getItem('token');
